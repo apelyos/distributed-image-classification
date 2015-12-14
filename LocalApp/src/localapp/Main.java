e33155ad73866f902e2f02f8f85d9bf406870730
Ben <Ben@Ben-PC>
2015-12-14 22:29:27 +0200

    Fixed input arguments & HTML output files, added logger support.

--- LocalApp/src/localapp/Main.java
+++ LocalApp/src/localapp/Main.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import dal.Configuration;
 import dal.NodesMgmt;
@@ -22,43 +23,57 @@ import messages.Summary;
 
 
 public class Main {
+	private static Logger logger = Logger.getLogger("Local App");
 
 	public static void main(String[] args) {
 		File imageFile;
 		int jobsPerWorker;
-		boolean terminate;
+		boolean terminate = false;
 		
-		if (args.length != 3)
-			throw new IllegalArgumentException("Exactly 3 parameters required!");
-		
+		if (args.length < 3 || args.length > 4)
+			throw new IllegalArgumentException("Invalid number of arguments entered");
+		else
 		    try {
-		        String fileLocation = args[0];
-		        jobsPerWorker = Integer.parseInt(args[1]);
-		        terminate = Boolean.parseBoolean(args[2]);
-		        imageFile = new File(fileLocation);
-		        start(imageFile, jobsPerWorker, terminate);
+		        String inputFile = args[0];
+		        String outputFile = args[1];
+		        jobsPerWorker = Integer.parseInt(args[2]);
+		        if (args.length == 3 && args[3].equals("terminate"))
+		        	terminate = true;
+		        imageFile = new File(inputFile);
+		        start(imageFile, outputFile, jobsPerWorker, terminate);
 		    } catch (IllegalArgumentException e) {
-		        System.err.println("Argument" + args[0] + " must be a string.");
-		        System.err.println("Argument" + args[1] + " must be an integer.");
-		        System.err.println("Argument" + args[2] + " must be a boolean.");
+		        System.err.println("Arguments" + args[0] + "and" + args[1] + " must be strings.");
+		        System.err.println("Argument" + args[2] + " must be an integer.");
 		        System.exit(1);
 		    } catch (Exception e) {e.printStackTrace();} 
 	}
 	
 	
-	public static void start (File images, int jobsPerWorker, boolean termination) throws Exception {
+	public static void start (File images, String outputFile, int jobsPerWorker, boolean termination) throws Exception {
 		Queue<Command> manageQueue = new Queue<Command>(Configuration.QUEUE_MANAGE, Command.class);
 		UUID key = UUID.randomUUID();
 		Storage s3_files = new Storage(Configuration.FILES_BUCKET_NAME);
+		logger.info("Uploads images files to S3");
 		String s3_key = s3_files.putFile(images);
+		logger.info("Creating command object");
 		Command start = createCommand(s3_key, jobsPerWorker, termination, key);
 		manageQueue.enqueueMessage(start);
-		NodesMgmt mgmt = new NodesMgmt(NodeType.MANAGEMENT);
-		mgmt.runInstance();
+		logger.info("Starting Manager");
+		runManager();
+		logger.info("Waiting for conclusion key");
 		String summaryKey = waitForConclusionKey();
 		BufferedReader s3Output = s3_files.get(summaryKey);
+		logger.info("Fetching summary object");
 		Summary summary = getSummary (s3Output);
-		createHTMLs(summary);
+		logger.info("Creating HTML output files");
+		createHTMLs(summary, outputFile);
+		logger.info("DONE!");
+	}
+	
+	private static void runManager() throws Exception {
+		NodesMgmt mgmt = new NodesMgmt(NodeType.MANAGEMENT);
+		if (mgmt.getNumberOfRunningInstances() == 0)
+			mgmt.runInstance();
 	}
 	
 	private static Command createCommand(String s3_key, int jobsPerWorker, boolean termination, UUID key) {
@@ -90,44 +105,32 @@ public class Main {
 	}
 	
 	
-	private static void createHTMLs (Summary summary) throws IOException {
+	private static void createHTMLs (Summary summary, String outputFile) throws IOException {
 		final String htmlHeader = "<!DOCTYPE html>\n<html>\n<body>\n";
-		final String htmlHFooter = "\n</body>\n</html>";
+		final String htmlFooter = "\n</body>\n</html>";
 		String mainHTMLBody = "";
-		File mainHTML = new File ("Main.html");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(mainHTML));
+		File mainHTML = new File (outputFile + ".html");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(mainHTML));	
 		
 		for (int i = 0; i < 5; i++) {
 			ImageSize curSize = ImageSize.values()[i];
+			logger.info("Processing " + curSize + " picture list to HTML output file");
 			if (summary.getListOfSize(curSize) == null) {
-				mainHTMLBody += "<h1>"+curSize.toString()+"</h1>\n";
-				mainHTMLBody += "<p>No pictures exists in this size</p>\n";
+				mainHTMLBody += "<h1>No pictures exists in size " + curSize.toString() + "</h1>\n";
 			} else {
-				File sizeHTML = new File (curSize.toString()+".html");
+				String fileName = outputFile + "_" +curSize.toString()+".html";
+				File sizeHTML = new File (fileName);
 				String sizedBody = "<h1>"+curSize.toString()+"</h1>\n";
-				mainHTMLBody += sizedBody;
+				mainHTMLBody += "<h1><a href=\" "+ fileName + "\">" + curSize.toString() +" Pictures</a></h1>\n";
 				BufferedWriter sizedBW = new BufferedWriter(new FileWriter(sizeHTML));
 				List<String> urls = summary.getListOfSize(curSize);
-				for (int j = 0; j < urls.size() ; j++) {
+				for (int j = 0; j < urls.size() ; j++)
 					sizedBody += "<a href=\"" + urls.get(j) + "\">\n<img src=\"" + urls.get(j) + "\"></a><br> \n";
-					mainHTMLBody += "<a href=\""+ urls.get(j) +"\">" + urls.get(j) + "</a><br> \n";
-				}
-				sizedBW.write(htmlHeader+sizedBody+htmlHFooter);
+				sizedBW.write(htmlHeader+sizedBody+htmlFooter);
 				sizedBW.close();
 			}
 		}
-		
-		mainHTMLBody += "<h1>"+ImageSize.DEAD.toString()+"</h1>\n";
-		if (summary.getListOfSize(ImageSize.DEAD) == null) 
-			mainHTMLBody += "<p>No pictures exists in this size</p>\n";
-		else {
-			List<String> urls = summary.getListOfSize(ImageSize.DEAD);
-			for (int j = 0; j < urls.size() ; j++) {
-				mainHTMLBody += "<a href=\""+ urls.get(j) +"\">" + urls.get(j) + "</a><br> \n";
-			}
-		}
-		
-		bw.write(htmlHeader+mainHTMLBody+htmlHFooter);
+		bw.write(htmlHeader+mainHTMLBody+htmlFooter);
 		bw.close();
 	}
 
