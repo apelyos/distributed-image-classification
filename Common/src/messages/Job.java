8b371a11af54cad3f1574297fe530506086f2144
apelyos <apel.yos@gmail.com>
2015-12-20 00:33:56 +0200

    Job now includes termination boolean

--- Common/src/messages/Job.java
+++ Common/src/messages/Job.java
@@ -8,16 +8,25 @@ import javax.xml.bind.annotation.*;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Job {
-	@SuppressWarnings("unused")
 	private Job () {
 		// for jaxb
+	}
+	
+	public static Job createTerminationJob() {
+		Job job = new Job();
+		job.terminate = true;
+		job.serialNumber = -1;
+		return job;
 	}
 	
 	public Job (String imgURL, int serial, UUID managerUuid) {
 		this.imageUrl = imgURL;
 		this.serialNumber = serial; 
 		this.managerUuid = managerUuid ;
+		this.terminate = false;
 	}
+	
+	public boolean terminate;
 	
     public int serialNumber;
     
